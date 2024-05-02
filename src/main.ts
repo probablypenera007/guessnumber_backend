a0e921312233648722c02e56fbf833cd66077d99
James Penera <jamespenera@icloud.com>
2024-05-03 01:23:51 +0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors());
+  
   app.enableCors({
     origin: 'http://localhost:3000',  
     allowedHeaders: 'Content-Type, Accept',
+    methods: 'GET,POST,PUT,DELETE,OPTIONS', 
+    credentials: true, 
   });
   
-  await app.listen(3001);
+  await app.listen(3001, () => console.log(`Server running on http://localhost:3001`));
 }
 bootstrap();
