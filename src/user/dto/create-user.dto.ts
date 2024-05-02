ee775992bb6a840499d7077c9d7af25346642e59
James Penera <jamespenera@icloud.com>
2024-05-02 21:09:36 +0400

    created user DTO

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,6 @@
+
+
+export class CreateUserDto {
+    readonly name: string;
+    readonly points: number;
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get,Post} from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './schemas/user.schema';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('players')
 export class UserController {
@@ -14,7 +15,7 @@ export class UserController {
   @Post()
     async createUser(
         @Body()
-        user,
+        user: CreateUserDto
     ): Promise<User> {
         return this.userService.create(user);
     }
