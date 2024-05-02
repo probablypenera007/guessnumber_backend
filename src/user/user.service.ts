9fbddbaa4c33a52676b6eb87b67addf5b4ec3d66
James Penera <jamespenera@icloud.com>
2024-05-02 20:58:23 +0400

    Update user.service.ts

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -8,18 +8,16 @@ export class UserService {
     constructor (
         @InjectModel(User.name) 
         private userModel: mongoose.Model<User>
-    ) {
-       
+    ) {}
+
+    async findAll(): Promise<User[]> {
+        const users = await this.userModel.find();
+        return users;
     }
 
     async create(user: User): Promise<User> {
         const createdUser = new this.userModel(user);
         return createdUser.save();
-    }
-
-    async findAll(): Promise<User[]> {
-        const users = await this.userModel.find();
-        return users;
     }
 }
 
